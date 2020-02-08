4ca2a46bacf1c2e42a8f3525f19568830263eef0
Ashwani Panchal <ashwani.panchal@hotmail.com>
2020-02-08 21:52:18 +0530

    just created app and done minor things

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import SecondPage from './components/SecondPage'
+
+function App() {
+  return (
+    <>
+    {/* <div className="App">
+      <header className="App-header">
+       <h1>Welcome to my Porfolio</h1>
+      </header>
+
+    </div> */}
+    {/* <div className="top-header-nav">
+      <h2 className="title">ASHWANI PANCHAL</h2>
+    </div> */}
+    <nav class="navbar navbar-dark bg-dark">
+    <h2 className="title">ASHWANI PANCHAL</h2>
+    
+    </nav>
+    <div className="bottom-bar">
+
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SecondPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"; 
+
+class SecondPage extends Component {
+  render() {
+    return (
+     <div>
+       <h2 className="my-work">My Work</h2>
+     </div>
+    );
+  }
+}
+
+export default SecondPage;
